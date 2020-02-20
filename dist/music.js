4a4387e80a00d8945d5b5f4d9c3e0fa271ecf272
LightningUZ2 <3348064478@qq.com>
2020-02-20 11:45:54 +0800

    Site updated: 2020-02-20 11:45:49

--- dist/music.js
+++ dist/music.js
@@ -14,6 +14,42 @@ const ap = new APlayer({
         artist: '瀧沢一留',
         url: 'http://music.163.com/song/media/outer/url?id=4956877.mp3',
         cover: 'http://p1.music.126.net/Li-XsFdYi7D00meK_2YCXQ==/5798824325014681.jpg',
+      },
+      {
+        name: '114514',
+        artist: 'Vicho',
+        url: 'http://music.163.com/song/media/outer/url?id=1305993204.mp3',
+        cover: 'http://p1.music.126.net/QMEby0oH57PgR94MkJG1Eg==/109951163522198846.jpg',
+      },
+      {
+        name: 'Pure',
+        artist: 'SCHAT10（影）',
+        url: 'http://music.163.com/song/media/outer/url?id=1419931389.mp3',
+        cover: 'http://p2.music.126.net/j3kW5i3VswZ2rqw4Zc11IQ==/109951164665017847.jpg',
+      },
+      {
+        name: 'Lemon Soda',
+        artist: 'NGC 3.14/Tenkitsune',
+        url: 'http://music.163.com/song/media/outer/url?id=410518637.mp3',
+        cover: 'http://p1.music.126.net/FTzYpffgZnCayUbmu23PbA==/1410673425530110.jpg',
+      },
+      {
+        name: 'summer vibe',
+        artist: 'Cyan Lpegd',
+        url: 'http://music.163.com/song/media/outer/url?id=532786740.mp3',
+        cover: 'http://p1.music.126.net/VjX98KnowcZ6dQJARiT_7g==/109951163116321305.jpg',
+      },
+      {
+        name: 'DJ Okawari - Flower Dance（钢琴原版）',
+        artist: 'Oturans',
+        url: 'http://music.163.com/song/media/outer/url?id=515788042.mp3',
+        cover: 'http://p2.music.126.net/9Nco9ph8lZUAR8f-ITl78Q==/109951163051630298.jpg',
+      },
+      {
+        name: '花降らし',
+        artist: 'n-buna/初音ミク',
+        url: 'http://music.163.com/song/media/outer/url?id=412902325.mp3',
+        cover: 'http://p1.music.126.net/Dev8qwDRGjIxAtopFG0uxg==/3263350512830591.jpg',
       }
     ]
 });
